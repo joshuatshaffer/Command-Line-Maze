d4a696d2c195b03e397fca32029e64a0b255a445
Joshua <joshua2@Joshuas-iMac.local>
2015-09-01 16:39:04 -0500

    Initial Commit

---
+++ cmd_line_maze/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cmd_line_maze
+//
+//  Created by Joshua on 9/1/15.
+//  Copyright (c) 2015 fartnarbuckle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
